4f5e9cb34fad906f0362e34a47d2247c122e84db
WannxxRd <ridwan.umsorong@gmail.com>
2025-10-30 18:56:59 +0900

    Second commit

--- app/Views/template.php
+++ app/Views/template.php
@@ -40,7 +40,7 @@
 
             <a href="<?= base_url() ?>" class="logo d-flex align-items-center me-auto">
                 <img src="<?= base_url('assets/img/logo.png') ?>" alt="Logo">
-                <h1 class="sitename">SISPANDALWAS</h1>
+                <h4 class="sitename">SISPANDALWAS</h4>
             </a>
 
             <?= view('menu') ?>
