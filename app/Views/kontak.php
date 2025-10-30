d7e70498d0cfb3945e5c4c428b937ce7169f44c9
WannxxRd <ridwan.umsorong@gmail.com>
2025-10-30 18:33:02 +0900

    Second commit

--- app/Views/kontak.php
+++ app/Views/kontak.php
@@ -23,7 +23,7 @@
     </div>
 </div>
 <br><br><br>
-<div class="container">
+<div class="container mt-2">
     <div class="row">
         <div class="col-12">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4104.867792926731!2d130.8152691682698!3d-0.43304412963984085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d5f1d22a3347cb7%3A0x58b653c13549814d!2sBLUD%20UPTD%20Pengelolaan%20KKP%20Raja%20Ampat!5e1!3m2!1sid!2sid!4v1761816354084!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
