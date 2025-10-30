030faf69f20f6385779fe9baf932040dbc67b665
WannxxRd <ridwan.umsorong@gmail.com>
2025-10-30 16:51:01 +0900

    Second

--- app/Views/kontak.php
+++ app/Views/kontak.php
@@ -26,7 +26,7 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.708793622138!2d119.6549266747572!3d-5.153797837603451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee32f6f6f4d7b%3A0x8e5f6c4b8e4e4e4e!2sUniversitas%20Muhammadiyah%20Sorong!5e0!3m2!1sid!2sid!4v1696351234567!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/place/BLUD+UPTD+Pengelolaan+KKP+Raja+Ampat/@-0.4330051,130.8139856,805m/data=!3m2!1e3!4b1!4m6!3m5!1s0x2d5f1d22a3347cb7:0x58b653c13549814d!8m2!3d-0.4330105!4d130.8165605!16s%2Fg%2F11dxpfg7ny?entry=ttu&g_ep=EgoyMDI1MTAyOC4wIKXMDSoASAFQAw%3D%3D" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 </div>
