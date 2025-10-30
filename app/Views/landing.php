86ca2386666ecb7ca288543d07c166a95e8de347
WannxxRd <ridwan.umsorong@gmail.com>
2025-10-30 18:51:27 +0900

    Second commit

--- app/Views/landing.php
+++ app/Views/landing.php
@@ -56,7 +56,7 @@
 <body class="index-page">
 
     <header id="header" class="header d-flex align-items-center fixed-top">
-        <div class="container-fluid container-l position-relative d-flex align-items-center">
+        <div class="container-fluid container-sm position-relative d-flex align-items-center">
 
             <a href="<?= base_url() ?>" class="logo d-flex align-items-center me-auto">
                 <img src="<?= base_url('assets/img/logo.png') ?>" alt="Logo">
